10f813fd9ca9766a595b99f6b14d42f1fc21d83a
Gaurav <gsatija17@gmail.com>
2020-08-18 00:37:13 -0700

    START01 in JAVA

---
+++ Beginner/START01.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+	    /* 
+	    Steps:
+	    	1. Create a scanner object to take input.
+		2. Take input from user into an integer variable
+		3. Output integer variable.
+	    */ 
+
+	    Scanner scanner = new Scanner(System.in);
+	    int input = scanner.nextInt();
+	    System.out.print(input);
+		
+	}
+}
