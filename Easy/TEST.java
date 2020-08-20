256a9afb54a1f486326256b409160a16f64c75c0
Gaurav <gsatija17@gmail.com>
2020-08-20 06:46:57 -0700

    TEST Solution in JAVA

---
+++ Easy/TEST.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+/** 
+ 	Codechef imgauravsatija
+	Github   iamgauravsatija
+*/
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		
+		while(input != 42){
+			System.out.println(input);
+			input = scanner.nextInt();
+		}
+
+	}
+}
+
